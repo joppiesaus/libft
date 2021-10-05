754631a651b454f0aca8de35fa7a585967c42074
Job van der Zweep <jobvan-d@student.codam.nl>
2021-10-05 13:49:51 +0200

    add tolower toupper

---
+++ ft_to_x.c
@@ -0,0 +1,17 @@
+int ft_toupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (c - ('a' - 'A'));
+	}
+	return (c);
+}
+
+int ft_tolower(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (c + ('a' - 'A'));
+	}
+	return (c);
+}
