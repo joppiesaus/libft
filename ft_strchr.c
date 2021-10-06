11bd3431c6a9bcee38349f4a11ce01076a49d943
Job van der Zweep <jobvan-d@student.codam.nl>
2021-10-06 11:35:39 +0200

    ft_strchr(also to be tested)

---
+++ ft_strchr.c
@@ -0,0 +1,19 @@
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != 0)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (c == 0)
+	{
+		return (s);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
