5603e6c1f6b68dff0f4ac6983621aae7d55ad1d1
Job van der Zweep <jobvan-d@student.codam.nl>
2021-10-06 16:32:42 +0200

    clarify ft_strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,5 +1,8 @@
 #include <stddef.h>
 
+/* intead of doing the classic integer counting,
+ * increment the pointer. this is slightly faster
+ * (probably has to do something with memory access or something idk) */
 size_t	ft_strlen(const char *s)
 {
 	const char	*start;
