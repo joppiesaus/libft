906eac4bfe46c9bc010fa9e60ef74bb0502f2fb6
Job van der Zweep <jobvan-d@student.codam.nl>
2021-10-08 20:08:06 +0200

    add ft_memchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -36,3 +36,19 @@ char	*ft_strrchr(const char *s, int c)
 	}
 	return (NULL);
 }
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((const char *)s)[i] == c)
+		{
+			return ((void *)(((const char *)s) + i));
+		}
+		i++;
+	}
+	return (NULL);
+}
--- libft.h
+++ libft.h
@@ -18,6 +18,7 @@ int ft_tolower(int c);
 int	ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
 
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
