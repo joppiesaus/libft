1dbdfae9977d34e22d8d854da2953f0f92825c27
Job van der Zweep <jobvan-d@student.codam.nl>
2021-10-12 17:27:06 +0200

    shorten strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,17 +6,11 @@ char	*ft_strdup(const char *s1)
 {
 	char	*s2;
 	size_t	len;
-	size_t	n;
 
 	len = ft_strlen(s1) + 1;
 	s2 = malloc(len);
 	if (s2 == NULL)
 		return (s2);
-	n = 0;
-	while (n < len)
-	{
-		s2[n] = s1[n];
-		n++;
-	}
+	ft_memcpy(s2, s1, len);
 	return (s2);
 }
