5d23e059f27f8b92438ff229820bd22023453035
Job van der Zweep <jobvan-d@student.codam.nl>
2021-10-14 18:38:59 +0200

    put strl* funcitons in separate files

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -2,25 +2,9 @@
 
 #include <limits.h>
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
-	size_t	n;
-
-	if (dstsize == 0)
-	{
-		return (ft_strlen(src));
-	}
-	n = 0;
-	while (n < dstsize - 1 && src[n] != 0)
-	{
-		dst[n] = src[n];
-		n++;
-	}
-	dst[n] = 0;
-	return (ft_strlen(src));
-}
-
-/* TODO: uses long, might overflow because of the size_t,
+/* Concats src to dst with a maximum of dstsize chars.
+ * returns the length of the string it intended to create.
+ * TODO: uses long, might overflow because of the size_t,
  * though who uses such large strings?
  * this is also why there is a protection built in for underflowing. */
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
