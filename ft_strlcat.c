ecc6adbeeee1d06e380e454d91f87702590111fc
Job van der Zweep <jobvan-d@student.codam.nl>
2021-10-21 16:38:47 +0200

    rewrite strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,37 +1,34 @@
 #include "libft.h"
 
-#include <limits.h>
-
-/* Concats src to dst with a maximum of dstsize chars.
- * returns the length of the string it intended to create.
- * TODO: uses long, might overflow because of the size_t,
- * though who uses such large strings?
- * this is also why there is a protection built in for underflowing. */
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+/* strlcpy straight up stolen from myself, but this one doesn't
+ * return the length, so we save an unnecessary strlen(src) operation.
+ * Also doesn't check if dstsize == 0. Probably nitpicking. */
+static void	m_strlcpy(char *dst, const char *src, const size_t dstsize)
 {
-	long	len;
-	long	n;
-	long	ldstsize;
+	size_t	n;
 
-	len = (long)ft_strlen(dst);
 	n = 0;
-	ldstsize = (long)dstsize;
-	if (ldstsize < 0)
+	while (n < dstsize - 1 && src[n] != 0)
 	{
-		ldstsize = LONG_MAX;
-	}
-	while (n < ldstsize - len - 1 && src[n] != 0)
-	{
-		dst[n + len] = src[n];
+		dst[n] = src[n];
 		n++;
 	}
-	if (ldstsize > 0)
+	dst[n] = 0;
+}
+
+/* Concats src to dst with a maximum of dstsize chars.
+ * returns the length of the string it intended to create. */
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	size_t	srclen;
+	size_t	dstlen;
+
+	srclen = ft_strlen(src);
+	dstlen = ft_strlen(dst);
+	if (dstlen >= dstsize)
 	{
-		dst[n + len] = 0;
+		return (dstsize + srclen);
 	}
-	if (len > ldstsize)
-	{
-		len = ldstsize;
-	}
-	return (len + ft_strlen(src));
+	m_strlcpy((dst + dstlen), src, dstsize - dstlen);
+	return (srclen + dstlen);
 }
